64fd69ea6cdb54107bec8f6dc26ba85f6d76cea7
Andreas <derzandrr03@gmail.com>
2023-07-30 17:42:05 +0700

    Menambahkan requirement package sqlite dan java.sql

--- CapitalisM/src/main/java/module-info.java
+++ CapitalisM/src/main/java/module-info.java
@@ -1,7 +1,8 @@
 module os.finance.capitalism {
     requires javafx.controls;
     requires javafx.fxml;
-            
+    requires org.xerial.sqlitejdbc;
+    requires java.sql;
                             
     opens os.finance.capitalism to javafx.fxml;
     exports os.finance.capitalism;
